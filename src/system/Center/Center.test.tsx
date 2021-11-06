e1f6644f28aca68d0d6892837806e47e29fe2941
Florian Pürschel <hello@prshl.de>
2021-11-06 12:06:38 +0100

    describe testcases

--- src/system/Center/Center.test.tsx
+++ src/system/Center/Center.test.tsx
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import Center from ".";
 
-test("renders a Center component", () => {
-  render(<Center>Sample Text</Center>);
-  const linkElement = screen.getByText("Sample Text");
-  expect(linkElement).toBeInTheDocument();
+describe("<Center />", () => {
+  test("renders a Center component", () => {
+    render(<Center>Sample Text</Center>);
+    const linkElement = screen.getByText("Sample Text");
+    expect(linkElement).toBeInTheDocument();
+  });
 });
--- src/system/text/text.test.tsx
+++ src/system/text/text.test.tsx
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import Text from ".";
 
-test("renders a Text component", () => {
-  render(<Text>Sample Text</Text>);
-  const linkElement = screen.getByText("Sample Text");
-  expect(linkElement).toBeInTheDocument();
+describe("<Text />", () => {
+  test("renders a Text component", () => {
+    render(<Text>Sample Text</Text>);
+    const linkElement = screen.getByText("Sample Text");
+    expect(linkElement).toBeInTheDocument();
+  });
 });
