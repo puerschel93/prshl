967c62a139fa69ad1c91c1cd8d01dbf7099fd2f7
Florian Pürschel <hello@prshl.de>
2021-11-07 23:33:07 +0100

    Add <Link /> as named export

--- src/system/style.ts
+++ src/system/style.ts
@@ -1,5 +1,6 @@
 import Center from "./center";
 import Text from "./text";
 import Box from "./box";
+import Link from "./link";
 
-export { Center, Text, Box };
+export { Center, Text, Box, Link };
