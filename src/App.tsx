358411861c30bd5b271877951eadb5dae9825b04
Florian Pürschel <hello@prshl.de>
2021-11-07 23:33:57 +0100

    Add Socialmedia / Github Links

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Center, Text } from "system/style";
+import { Center, Text, Box, Link } from "system/style";
 import { useBreakpointValue } from "@chakra-ui/media-query";
 
 const App = () => {
@@ -17,12 +17,31 @@ const App = () => {
       zIndex={2}
       flexDirection="column"
     >
-      <Text color="salmon.500" fontSize="3xl" fontWeight="bold">
+      <Text color="salmon.300" fontSize="3xl" fontWeight="bold">
         {content}
       </Text>
+
       <Text color="white" fontSize="lg" fontWeight="bold" marginTop="1rem">
         私を雇う
       </Text>
+
+      <Box
+        width="50vw"
+        flexDirection="row"
+        justifyContent="center"
+        marginTop="1rem"
+      >
+        <Link href="https://github.com/puerschel93" mr="1rem">
+          Github
+        </Link>
+        <Link href="https://www.linkedin.com/in/puerschel" mr="1rem">
+          LinkedIn
+        </Link>
+        <Link href="https://www.instagram.com/mr.prshl/" mr="1rem">
+          Instagram
+        </Link>
+        <Link href="mailto:hello@prshl.de">Mail</Link>
+      </Box>
     </Center>
   );
 };
