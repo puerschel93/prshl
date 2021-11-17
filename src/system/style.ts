07ee0918a36de0e17111a72365feba193cefef70
Florian Pürschel <hello@prshl.de>
2021-11-17 22:15:40 +0100

    Update style.ts

--- src/system/style.ts
+++ src/system/style.ts
@@ -2,5 +2,6 @@ import Center from "./center";
 import Text from "./text";
 import Box from "./box";
 import Link from "./link";
+import Flex from "./flex";
 
-export { Center, Text, Box, Link };
+export { Center, Text, Box, Link, Flex };
