a0fb31cc2355ba8dc837ebc68bae11e9d211fde5
Florian Pürschel <hello@prshl.de>
2022-03-07 20:24:59 +0100

    remove strictmdoe

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "system/global";
 import theme from "system/theme";
-import App from "./App";
+import { App } from "./app";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>,
   document.getElementById("root")
 );
